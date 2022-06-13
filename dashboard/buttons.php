72de513e5d108eeba31c11f639ddafbc699d700b
Fapaccha1 <fapaccha@utpl.edu.ec>
2022-06-12 21:26:57 -0500

    Carga de Archivo Principal

---
+++ dashboard/buttons.php
@@ -0,0 +1,11 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Pagina de Visitantes</h1>
+
+    <img src="img/im1.jpg">
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "vistas/parte_inferior.php"?>
